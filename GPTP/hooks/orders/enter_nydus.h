55d886af2f6e6a7a7aee247f61314549f7ee45cf
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-05-31 12:05:20 +0200

    Version 5.900

--- GPTP/hooks/orders/enter_nydus.h
+++ GPTP/hooks/orders/enter_nydus.h
@@ -3,9 +3,9 @@
 
 namespace hooks {
 
-	bool canEnterNydusCanal(CUnit* unit, CUnit* nydusCanal);		//004E8C20
-	void enterNydusCanal_Effect(CUnit* unit, CUnit* nydusCanal);	//004EA180
-	void orders_EnterNydusCanal(CUnit* unit);						//004EA3E0
+	bool canEnterNydusCanal(CUnit* unit, CUnit* nydusCanal);		//E8C20
+	void enterNydusCanal_Effect(CUnit* unit, CUnit* nydusCanal);	//EA180
+	void orders_EnterNydusCanal(CUnit* unit);						//EA3E0
 
 	void injectEnterNydusHooks();
 
