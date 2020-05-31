f15501353da2f2a7b596855c5a19495e4cc8db3a
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-05-31 14:08:58 +0200

    Version 5.900

--- GPTP/hooks/interface/selection.h
+++ GPTP/hooks/interface/selection.h
@@ -3,8 +3,11 @@
 
 namespace hooks {
 
-	void function_0046FB40(CUnit* clicked_unit);
-	u32 SortAllUnits(CUnit* unit,CUnit** unit_list,CUnit** units_in_bounds);
+	u32 SortAllUnits(CUnit* unit,CUnit** unit_list,CUnit** units_in_bounds);							//6F0F0
+	u32 combineSelectionsLists(CUnit* unit,CUnit** unit_list_1,CUnit** unit_list_2,u32 list_length);	//6F290
+	void getSelectedUnitsInBox(Box16* coords);															//6FA40
+	void getSelectedUnitsAtPoint(u32 unk1, u32 unk2);													//6FB40
+	void selectUnitGroup(u32 selectionGroupNumber);														//96B40
 
 	void injectSelectMod();
 
