3487df1f5cfecd2841a4c9ebe08c06ebc52f81fc
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-05-31 10:53:00 +0200

    Version 5.900

--- GPTP/hooks/interface/btns_cond.cpp
+++ GPTP/hooks/interface/btns_cond.cpp
@@ -3,29 +3,25 @@
 
 namespace hooks {
 
-#define BUTTON_ENABLED 1
-#define BUTTON_HIDDEN 0
-#define BUTTON_DISABLED -1
-
 /// These hooks are a selection of buttons conditions
 /// showing how they work, and allowing these cases
 /// to be modified for a specific unit or situation.
 /// No more buttons conditions will be hooked, so
 /// use these for anything you need.
 
-s32 BTNSCOND_NoNydusExit(CUnit* unit) {
-	return (unit->building.nydusExit == NULL) ? BUTTON_ENABLED : BUTTON_HIDDEN;
+s32 BTNSCOND_NoNydusExit(CUnit* unit, u16 reqVar, u32 playerId) {
+	return (unit->building.nydusExit == NULL) ? BUTTON_STATE::Enabled : BUTTON_STATE::Invisible;
 }
 
 ;
 
-s32 BTNSCOND_Movement(CUnit* unit) {
+s32 BTNSCOND_Movement(CUnit* unit, u16 reqVar, u32 playerId) {
 
-	s32 return_value = BUTTON_ENABLED;
+	s32 return_value = BUTTON_STATE::Enabled;
 
-	for(int i = 0;return_value == BUTTON_ENABLED && i < SELECTION_ARRAY_LENGTH;i++)
+	for(int i = 0;return_value == BUTTON_STATE::Enabled && i < SELECTION_ARRAY_LENGTH;i++)
 		if(clientSelectionGroup->unit[i] != NULL && clientSelectionGroup->unit[i]->status & UnitStatus::Burrowed)
-			return_value = BUTTON_HIDDEN;
+			return_value = BUTTON_STATE::Invisible;
 
 	return return_value;
 
@@ -33,23 +29,35 @@ s32 BTNSCOND_Movement(CUnit* unit) {
 
 ;
 
-s32 BTNSCOND_HasScarabs(CUnit* unit) {
+s32 BTNSCOND_HasScarabs(CUnit* unit, u16 reqVar, u32 playerId) {
 
-	s32 return_value = BUTTON_DISABLED;
+	s32 return_value = BUTTON_STATE::Disabled;
 
-	for(int i = 0;return_value == BUTTON_DISABLED && i < SELECTION_ARRAY_LENGTH;i++)
+	for(int i = 0;return_value == BUTTON_STATE::Disabled && i < SELECTION_ARRAY_LENGTH;i++)
 		if(	clientSelectionGroup->unit[i] != NULL && 
 			(clientSelectionGroup->unit[i]->id == UnitId::Hero_Warbringer || clientSelectionGroup->unit[i]->id == UnitId::ProtossReaver) &&
 			clientSelectionGroup->unit[i]->carrier.inHangarCount != 0
 		)
-			return_value = BUTTON_ENABLED;
+			return_value = BUTTON_STATE::Enabled;
 
 	return return_value;
 
 }
 
-#undef BUTTON_ENABLED
-#undef BUTTON_HIDDEN
-#undef BUTTON_DISABLED 
+s32 BTNSCOND_CanBuildUnit(CUnit* unit, u16 reqVar, u32 playerId) {
+
+	s32 return_value = BUTTON_STATE::Invisible;
+
+	if (
+		*clientSelectionCount <= 1 ||
+		unit->id == UnitId::ZergLarva ||
+		unit->id == UnitId::ZergMutalisk ||
+		unit->id == UnitId::ZergHydralisk
+	)
+		return_value = unit->canMakeUnit(reqVar, playerId);
+
+	return return_value;
+
+}
 
 } //hooks
