da37e16ff0c842474af8182b9964d4cb716e5d55
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-05-31 15:14:29 +0200

    Version 5.900

--- GPTP/hooks/utils/utils.h
+++ GPTP/hooks/utils/utils.h
@@ -3,9 +3,9 @@
 
 namespace hooks {
 
-void InitializeDialog(BinDlg* dialog, void* dlgFunc);	//00419D20
-bool unit_IsStandardAndMovable(CUnit* unit);			//0047B770
-CUnit* getActivePlayerFirstSelection();					//0049A850
+void InitializeDialog(BinDlg* dialog, void* dlgFunc);	//19D20
+bool unit_IsStandardAndMovable(CUnit* unit);			//7B770
+CUnit* getActivePlayerFirstSelection();					//9A850
 
 void injectUtilsHooks();
 
