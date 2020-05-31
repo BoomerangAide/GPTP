d7394201ab8b54322de83b98cb9238b9070067d9
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-05-31 11:33:58 +0200

    Version 5.900

--- GPTP/hooks/recv_commands/CMDRECV_Build.h
+++ GPTP/hooks/recv_commands/CMDRECV_Build.h
@@ -3,10 +3,11 @@
 
 namespace hooks {
 
-bool CMDRECV_PlaceBuildingAllowed(CUnit* builder, u8 orderId, u16 builtUnitId);	//0048DBD0
-void cmdRECV_PlaceBuildingNormal(s16 x, s16 y, u32 orderId, u32 builtUnitId);	//0048E010
-void cmdRECV_PlaceBuildingAddon(s16 x, s16 y, u32 orderId, u32 builtUnitId);	//0048E0A0
-void CMDRECV_Build(u8 orderId, s16 x, s16 y, u16 builtUnitId);					//004C23C0
+bool CMDRECV_PlaceBuildingAllowed(CUnit* builder, u8 orderId, u16 builtUnitId);					//8DBD0
+void cmdRECV_PlaceBuildingOrder(CUnit* builder, u32 builtUnitId, Point16 coords, u32 orderId);	//8DE70
+void cmdRECV_PlaceBuildingNormal(s16 x, s16 y, u32 orderId, u32 builtUnitId);					//8E010
+void cmdRECV_PlaceBuildingAddon(s16 x, s16 y, u32 orderId, u32 builtUnitId);					//8E0A0
+void CMDRECV_Build(u8 orderId, s16 x, s16 y, u16 builtUnitId);									//C23C0
 
 void injectCMDRECV_BuildHooks();
 
