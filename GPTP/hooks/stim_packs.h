ccbc6c8819299ed5d281aec77836b791e8815bf9
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-05-31 14:43:54 +0200

    Version 5.900

--- GPTP/hooks/stim_packs.h
+++ GPTP/hooks/stim_packs.h
@@ -1,13 +1,10 @@
 #pragma once
 #include "../SCBW/structures/CUnit.h"
 
-//V241 for VS2008
-
 namespace hooks {
 
-void CMDACT_Stimpack();					//004234D0
-void useStimPacksAIHook(CUnit* unit);	//004554A0
-void CMDRECV_StimPack();				//004C2F30
+void CMDACT_Stimpack();					//234D0
+void useStimPacksAIHook(CUnit* unit);	//554A0
 
 void injectStimPacksHooks();
 
