dd29a48af3b21643cdcea0f719bb6f1ea0ad9e87
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-05-31 11:51:47 +0200

    Version 5.900

--- GPTP/configure.cpp
+++ GPTP/configure.cpp
@@ -1,8 +1,6 @@
 #include "definitions.h"
 #include "Plugin.h"
 
-//V241 for VS2008
-
 //Called when the user hits the configure button in MPQDraft or FireGraft
 BOOL WINAPI Plugin::Configure(HWND hParentWnd) {
   /*
@@ -15,11 +13,7 @@ BOOL WINAPI Plugin::Configure(HWND hParentWnd) {
   MessageBox(
     hParentWnd,
     PLUGIN_NAME " (ID: " STR(PLUGIN_ID) ")"
-    "\nMade by pastelmind"
     "\nBuilt on " __DATE__ " " __TIME__
-    "\n"
-    "\n[Features]"
-    "\n - Add your own text here"
     ,
     PLUGIN_NAME,
     MB_TASKMODAL
