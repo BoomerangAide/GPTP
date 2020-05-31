dd0a34a16579a47303b59d93b666ac80d6ec90b7
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-05-31 11:32:23 +0200

    Version 5.900

--- GPTP/hooks/recv_commands/CMDRECV_Build.cpp
+++ GPTP/hooks/recv_commands/CMDRECV_Build.cpp
@@ -5,11 +5,14 @@
 
 namespace {
 
+bool hasSuppliesForUnit(u32 unitId, u32 playerId, Bool32 canShowErrorMessage);									//2CF70
+void refundAllQueueSlots(CUnit* unit);																			//66E80
+bool HasMoneyCanMake(CUnit* builder, u32 unitToBuild);															//67250
 Bool32 OrderAllowed(CUnit* unit, u16 order, u32 nationID);														//6DC20
 u32 function_00473FB0(CUnit* unit, u8 playerId, int x, int y, u16 unitId, u8 unk1, u8 unk2, u8 unk3, u8 unk4 );	//73FB0
 bool placeBuildingMsg(u32 result_function_473FB0);																//8D930
 bool CMDRECV_PlaceBuildingAllowedHelper(CUnit* unit, u8 orderId, u16 builtUnitId);								//8DBD0
-void function_0048DE70(CUnit* unit, u32 orderId, u16 dimensionX, u16 dimensionY, u32 builtUnitId);				//8DE70
+void function_0048DE70_Helper(CUnit* unit, u32 orderId, u16 dimensionX, u16 dimensionY, u32 builtUnitId);		//8DE70
 void cmdRECV_PlaceBuilding(s16 x, s16 y, u8 orderId, u16 builtUnitId);											//8E190
 
 } //unnamed namespace
@@ -52,6 +55,77 @@ bool CMDRECV_PlaceBuildingAllowed(CUnit* builder, u8 orderId, u16 builtUnitId) {
 
 	return bReturnValue;
 
+}
+
+;
+
+//function_0048DE70, assign an actual order to the builder for construction
+void cmdRECV_PlaceBuildingOrder(CUnit* builder, u32 builtUnitId, Point16 coords, u32 orderId) {
+
+	const u8*	CGame_player_race	=  (u8*)0x0057F1E2; //see RaceId enum
+	s32* const	s32_006CA4EC		= (s32*)0x006CA4EC;	//store a gas cost for a player
+	s32* const	s32_006CA51C		= (s32*)0x006CA51C;	//store a mineral cost for a player
+
+	if (orderId == OrderId::BuildingLand)
+		builder->orderTo(OrderId::BuildingLand, coords.x, coords.y);
+	else
+	{
+
+		bool bJumpToEnd = false;
+
+		if (
+			builder->id == UnitId::ZergDrone &&
+			(
+				((builder->mainOrderId == OrderId::DroneLand) && (builder->status & (UnitStatus::NoBrkCodeStart + UnitStatus::CanNotReceiveOrders))) ||
+				(builder->mainOrderId == OrderId::DroneBuild)
+			)
+		)
+			bJumpToEnd = true;
+
+		if (!bJumpToEnd) {
+
+			if (
+				((builder->id == UnitId::ZergNydusCanal) && (orderId == OrderId::Build5)) ||
+				(orderId == OrderId::CTFCOP2)
+			)
+				builder->orderTo(orderId,coords.x,coords.y);
+			else
+			{
+				
+				s32_006CA51C[builder->playerId] = units_dat::MineralCost[builtUnitId];
+				s32_006CA4EC[builder->playerId] = units_dat::GasCost[builtUnitId];
+
+				if (hasSuppliesForUnit(builtUnitId, builder->playerId, 1)) {
+
+					if(resources->minerals[builder->playerId] < s32_006CA51C[builder->playerId]) //Not enough minerals...mine more minerals.<0>
+						scbw::showErrorMessageWithSfx(builder->playerId,0x352,SoundId::Zerg_Advisor_ZAdErr00_WAV_2 + *CGame_player_race);
+					else
+					if(resources->gas[builder->playerId] < s32_006CA4EC[builder->playerId]) //Not enough Vespene gases....harvest more gas.<0>
+						scbw::showErrorMessageWithSfx(builder->playerId,0x353,SoundId::Zerg_Advisor_ZAdErr01_WAV + *CGame_player_race);
+					else {
+
+						builder->orderTo(orderId, coords.x, coords.y);
+
+						if (
+							(units_dat::BaseProperty[builder->id] & UnitProperty::Building) &&
+							(units_dat::BaseProperty[builtUnitId] & UnitProperty::Building)
+						)
+							builder->building.addonBuildType = builtUnitId;
+						else
+						{
+							refundAllQueueSlots(builder);
+							HasMoneyCanMake(builder, builtUnitId); //start construction if successful
+						}
+
+					}
+
+				}
+
+			}
+
+		}
+
+	}
 }
 
 ;
@@ -99,7 +173,7 @@ void cmdRECV_PlaceBuildingNormal(s16 x, s16 y, u32 orderId, u32 builtUnitId) {
 
 		DimensionY += (y * 32);
 
-		function_0048DE70(builder,orderId,DimensionX,DimensionY,builtUnitId);
+		function_0048DE70_Helper(builder,orderId,DimensionX,DimensionY,builtUnitId);
 
 	}
 
@@ -191,7 +265,7 @@ void cmdRECV_PlaceBuildingAddon(s16 x, s16 y, u32 orderId, u32 builtUnitId) {
 
 		DimensionY += addonPlaceY;
 
-		function_0048DE70(builder,orderId,DimensionX,DimensionY,builtUnitId);
+		function_0048DE70_Helper(builder,orderId,DimensionX,DimensionY,builtUnitId);
 
 	}
 
@@ -230,6 +304,61 @@ void CMDRECV_Build(u8 orderId, s16 x, s16 y, u16 builtUnitId) {
 //-------- Helper function definitions. Do NOT modify! --------//
 
 namespace {
+
+const u32 Func_hasSuppliesForUnit = 0x0042CF70;
+bool hasSuppliesForUnit(u32 unitId, u32 playerId, Bool32 canShowErrorMessage) {
+
+	static Bool32 bPreResult;
+
+	__asm {
+		PUSHAD
+		PUSH canShowErrorMessage
+		PUSH unitId
+		PUSH playerId
+		CALL Func_hasSuppliesForUnit
+		MOV bPreResult, EAX
+		POPAD
+	}
+
+	return (bPreResult != 0);
+
+}
+
+;
+
+const u32 Func_RefundAllQueueSlots = 0x00466E80;
+void refundAllQueueSlots(CUnit* unit) {
+
+	__asm {
+		PUSHAD
+		MOV EAX, unit
+		CALL Func_RefundAllQueueSlots
+		POPAD
+	}
+
+}
+
+;
+
+const u32 Func_HasMoneyCanMake = 0x00467250;
+bool HasMoneyCanMake(CUnit* builder, u32 unitToBuild) {
+
+	static Bool32 bPreResult;
+
+	__asm {
+		PUSHAD
+		PUSH unitToBuild
+		MOV EDI, builder
+		CALL Func_HasMoneyCanMake
+		MOV bPreResult, EAX
+		POPAD
+	}
+
+	return (bPreResult != 0);
+
+}
+
+;
 
 const u32 Func_OrderAllowed = 0x0046DC20;
 Bool32 OrderAllowed(CUnit* unit, u16 order, u32 nationID) {
@@ -326,7 +455,7 @@ bool CMDRECV_PlaceBuildingAllowedHelper(CUnit* unit, u8 orderId, u16 builtUnitId
 ;
 
 const u32 Func_Sub48DE70 = 0x0048DE70;
-void function_0048DE70(CUnit* unit, u32 orderId, u16 dimensionX, u16 dimensionY, u32 builtUnitId) {
+void function_0048DE70_Helper(CUnit* unit, u32 orderId, u16 dimensionX, u16 dimensionY, u32 builtUnitId) {
 
 	static Point16 dim;
 
