fe3815082ce923ac6baf8fa91e8621af6e8c9be5
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-05-31 10:54:47 +0200

    Version 5.900

--- GPTP/hooks/interface/btns_cond_inject.cpp
+++ GPTP/hooks/interface/btns_cond_inject.cpp
@@ -6,17 +6,27 @@ namespace {
 void __declspec(naked) BTNSCOND_NoNydusExit_Wrapper() {
 
 	static CUnit* unit;
+	static u16 reqVar;
+	static u32 playerId;
+
 	static s32 button_state;
 
 	__asm {
+
 		PUSH EBP
 		MOV EBP, ESP
+
 		MOV EAX, [EBP+0x08]
 		MOV unit, EAX
+
+		MOV reqVar, CX
+		MOV playerId, EDX
+
 		PUSHAD
+
 	}
 
-	button_state = hooks::BTNSCOND_NoNydusExit(unit);
+	button_state = hooks::BTNSCOND_NoNydusExit(unit, reqVar, playerId);
 
 	__asm {
 		POPAD
@@ -32,17 +42,27 @@ void __declspec(naked) BTNSCOND_NoNydusExit_Wrapper() {
 void __declspec(naked) BTNSCOND_Movement_Wrapper() {
 
 	static CUnit* unit;
+	static u16 reqVar;
+	static u32 playerId;
+
 	static s32 button_state;
 
 	__asm {
+
 		PUSH EBP
 		MOV EBP, ESP
+
 		MOV EAX, [EBP+0x08]
 		MOV unit, EAX
+
+		MOV reqVar, CX
+		MOV playerId, EDX
+
 		PUSHAD
+
 	}
 
-	button_state = hooks::BTNSCOND_Movement(unit);
+	button_state = hooks::BTNSCOND_Movement(unit, reqVar, playerId);
 
 	__asm {
 		POPAD
@@ -58,17 +78,63 @@ void __declspec(naked) BTNSCOND_Movement_Wrapper() {
 void __declspec(naked) BTNSCOND_HasScarabs_Wrapper() {
 
 	static CUnit* unit;
+	static u16 reqVar;
+	static u32 playerId;
+
 	static s32 button_state;
 
 	__asm {
+
 		PUSH EBP
 		MOV EBP, ESP
+
 		MOV EAX, [EBP+0x08]
 		MOV unit, EAX
+
+		MOV reqVar, CX
+		MOV playerId, EDX
+
 		PUSHAD
+
 	}
 
-	button_state = hooks::BTNSCOND_HasScarabs(unit);
+	button_state = hooks::BTNSCOND_HasScarabs(unit, reqVar, playerId);
+
+	__asm {
+		POPAD
+		MOV EAX, button_state
+		POP EBP
+		RETN 4
+	}
+
+}
+
+;
+
+void __declspec(naked) BTNSCOND_CanBuildUnit_Wrapper() {
+
+	static CUnit* unit;
+	static u16 reqVar;
+	static u32 playerId;
+
+	static s32 button_state;
+
+	__asm {
+
+		PUSH EBP
+		MOV EBP, ESP
+
+		MOV EAX, [EBP+0x08]
+		MOV unit, EAX
+
+		MOV reqVar, CX
+		MOV playerId, EDX
+
+		PUSHAD
+
+	}
+
+	button_state = hooks::BTNSCOND_CanBuildUnit(unit, reqVar, playerId);
 
 	__asm {
 		POPAD
@@ -89,6 +155,7 @@ void injectBtnsCondHook() {
 	jmpPatch(BTNSCOND_NoNydusExit_Wrapper,	0x00428340, 1);
 	jmpPatch(BTNSCOND_Movement_Wrapper,		0x004283C0, 0);
 	jmpPatch(BTNSCOND_HasScarabs_Wrapper,	0x004286A0, 0);
+	jmpPatch(BTNSCOND_CanBuildUnit_Wrapper,	0x00428E60, 0);
 }
 
 }
