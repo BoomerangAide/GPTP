77ba85696d761b23a6725a8b3107013247cd71e6
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-05-31 10:53:48 +0200

    Version 5.900

--- GPTP/hooks/interface/btns_cond.h
+++ GPTP/hooks/interface/btns_cond.h
@@ -3,9 +3,10 @@
 
 namespace hooks {
 
-s32 BTNSCOND_NoNydusExit(CUnit* unit);		//00428340
-s32 BTNSCOND_Movement(CUnit* unit);			//004283C0
-s32 BTNSCOND_HasScarabs(CUnit* unit);		//004286A0
+s32 BTNSCOND_NoNydusExit(CUnit* unit, u16 reqVar, u32 playerId);	//00428340
+s32 BTNSCOND_Movement(CUnit* unit, u16 reqVar, u32 playerId);		//004283C0
+s32 BTNSCOND_HasScarabs(CUnit* unit, u16 reqVar, u32 playerId);		//004286A0
+s32 BTNSCOND_CanBuildUnit(CUnit* unit, u16 reqVar, u32 playerId);	//00428E60
 
 //The injector function that should be called in initialize.cpp
 void injectBtnsCondHook();
