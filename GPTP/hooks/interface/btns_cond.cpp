67a6f97f58155bdf1bd0ea6941b58da9d24be9b0
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-07-28 10:08:09 +0200

    Version 5.910

--- GPTP/hooks/interface/btns_cond.cpp
+++ GPTP/hooks/interface/btns_cond.cpp
@@ -6,8 +6,9 @@ namespace hooks {
 /// These hooks are a selection of buttons conditions
 /// showing how they work, and allowing these cases
 /// to be modified for a specific unit or situation.
-/// No more buttons conditions will be hooked, so
-/// use these for anything you need.
+/// Not many more buttons conditions should need to be hooked,
+/// you should be able to use these for anything you need with
+/// internal checks for various cases to handle.
 
 s32 BTNSCOND_NoNydusExit(CUnit* unit, u16 reqVar, u32 playerId) {
 	return (unit->building.nydusExit == NULL) ? BUTTON_STATE::Enabled : BUTTON_STATE::Invisible;
@@ -43,6 +44,8 @@ s32 BTNSCOND_HasScarabs(CUnit* unit, u16 reqVar, u32 playerId) {
 	return return_value;
 
 }
+	
+;
 
 s32 BTNSCOND_CanBuildUnit(CUnit* unit, u16 reqVar, u32 playerId) {
 
@@ -59,5 +62,89 @@ s32 BTNSCOND_CanBuildUnit(CUnit* unit, u16 reqVar, u32 playerId) {
 	return return_value;
 
 }
+	
+				unit->id == UnitId::Hero_SamirDuran ||
+				unit->id == UnitId::Hero_InfestedDuran ||
+				unit->id == UnitId::Hero_InfestedKerrigan
+			)
+				techId = TechId::PersonnelCloaking;
+			else
+			if (unit->id == UnitId::TerranWraith || unit->id == UnitId::Hero_TomKazansky)
+				techId = TechId::CloakingField;
+			else
+				techId = TechId::None;
+
+			if (unit_from_selection->canUseTech(techId, playerId) == 1) {
+
+				if (
+					unit_from_selection->status & UnitStatus::CloakingForFree &&
+					!(unit_from_selection->status & UnitStatus::Burrowed)
+				)
+					returnValue = BUTTON_STATE::Invisible;
+				else
+				if (!(unit_from_selection->status & UnitStatus::RequiresDetection))
+					returnValue = BUTTON_STATE::Invisible;
+
+			}
+			else
+				returnValue = BUTTON_STATE::Invisible;
+
+		}
+
+	}
+
+	return returnValue;
+
+}
+
+;
+
+s32 BTNSCOND_CanCloak(CUnit* unit, u16 reqVar, u32 playerId) {
+	
+	s32 returnValue = BUTTON_STATE::Invisible;
+	CUnit* unit_from_selection;
+
+	for(int i = 0; i < SELECTION_ARRAY_LENGTH && (returnValue == BUTTON_STATE::Invisible); i++) {
+
+		unit_from_selection = clientSelectionGroup->unit[i];
+
+		if(unit_from_selection != NULL) {
+
+			u8 techId; 
+
+			if (
+				unit->id == UnitId::TerranGhost ||
+				unit->id == UnitId::Hero_SarahKerrigan ||
+				unit->id == UnitId::Hero_AlexeiStukov ||
+				unit->id == UnitId::Hero_SamirDuran ||
+				unit->id == UnitId::Hero_InfestedDuran ||
+				unit->id == UnitId::Hero_InfestedKerrigan
+			)
+				techId = TechId::PersonnelCloaking;
+			else
+			if (unit->id == UnitId::TerranWraith || unit->id == UnitId::Hero_TomKazansky)
+				techId = TechId::CloakingField;
+			else
+				techId = TechId::None;
+
+			if(
+				unit_from_selection->canUseTech(techId,playerId) == 1 &&			//must be able to use cloaking tech for more checks to be necessary
+				(
+					!(unit_from_selection->status & UnitStatus::CloakingForFree) ||	//to cloak, unit should not already be cloaked for free
+					unit_from_selection->status & UnitStatus::Burrowed				//if burrowed, cloaking for free, but still allowed to cloak
+				) &&
+				!(unit_from_selection->status & UnitStatus::RequiresDetection)		//if unit doesn't already require detection
+			)
+				returnValue = BUTTON_STATE::Enabled;
+
+		}
+
+	}
+
+	return returnValue;
+
+}
+
+;	
 
 } //hooks
