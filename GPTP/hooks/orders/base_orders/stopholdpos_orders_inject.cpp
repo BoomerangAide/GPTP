55431ed17ec8f273b2eb69e8f52617369ddf7961
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-09-08 15:01:00 +0200

    Version 5.920

--- GPTP/hooks/orders/base_orders/stopholdpos_orders_inject.cpp
+++ GPTP/hooks/orders/base_orders/stopholdpos_orders_inject.cpp
@@ -4,100 +4,121 @@
 
 namespace {
 
-	void __declspec(naked) orders_MedicHoldPositionWrapper() {
+void __declspec(naked) orders_MedicHoldPositionWrapper() {
 
-		static CUnit* unit;
+	static CUnit* unit;
 
-		__asm {
-			MOV unit, ESI
-			PUSHAD
-		}
-
-		hooks::orders_MedicHoldPosition(unit);
-
-		__asm {
-			POPAD
-			RETN
-		}
-
+	__asm {
+		MOV unit, ESI
+		PUSHAD
 	}
 
-	;
+	hooks::orders_MedicHoldPosition(unit);
 
-	void __declspec(naked) orders_ReaverStopWrapper() {
-
-		static CUnit* unit;
-
-		__asm {
-			PUSH EBP
-			MOV EBP, ESP
-			MOV EAX, [EBP+0x08]
-			MOV unit, EAX
-			PUSHAD
-		}
-
-		hooks::orders_ReaverStop(unit);
-
-		__asm {
-			POPAD
-			MOV ESP, EBP
-			POP EBP
-			RETN 4
-		}
-
+	__asm {
+		POPAD
+		RETN
 	}
 
-	;
+}
 
-	void __declspec(naked) orders_GuardWrapper() {
+;
 
-		static CUnit* unit;
+void __declspec(naked) orders_ReaverStopWrapper() {
 
-		__asm {
-			MOV unit, ESI
-			PUSHAD
-		}
+	static CUnit* unit;
 
-		hooks::orders_Guard(unit);
-
-		__asm {
-			POPAD
-			RETN
-		}
-
+	__asm {
+		PUSH EBP
+		MOV EBP, ESP
+		MOV EAX, [EBP+0x08]
+		MOV unit, EAX
+		PUSHAD
 	}
 
-	;
+	hooks::orders_ReaverStop(unit);
 
-	void __declspec(naked) function_004774A0_Wrapper() {
-
-		static CUnit* unit;
-
-		__asm {
-			MOV unit, EAX
-			PUSHAD
-		}
-
-		hooks::function_004774A0(unit);
-
-		__asm {
-			POPAD
-			RETN
-		}
-
+	__asm {
+		POPAD
+		MOV ESP, EBP
+		POP EBP
+		RETN 4
 	}
 
-	;
+}
+
+;
+
+void __declspec(naked) orders_CarrierStopWrapper() {
+
+	static CUnit* unit;
+
+	__asm {
+		MOV unit, EAX
+		PUSHAD
+	}
+
+	hooks::orders_CarrierStop(unit);
+
+	__asm {
+		POPAD
+		RETN
+	}
+
+}
+
+;
+
+void __declspec(naked) orders_GuardWrapper() {
+
+	static CUnit* unit;
+
+	__asm {
+		MOV unit, ESI
+		PUSHAD
+	}
+
+	hooks::orders_Guard(unit);
+
+	__asm {
+		POPAD
+		RETN
+	}
+
+}
+
+;
+
+void __declspec(naked) function_004774A0_Wrapper() {
+
+	static CUnit* unit;
+
+	__asm {
+		MOV unit, EAX
+		PUSHAD
+	}
+
+	hooks::orders_PlayerGuard(unit);
+
+	__asm {
+		POPAD
+		RETN
+	}
+
+}
+
+;
 
 }//unnamed namespace
 
 namespace hooks {
 
-	void injectStopHoldPosOrdersHooks() {
-		jmpPatch(orders_MedicHoldPositionWrapper,	0x00464050, 0);
-		jmpPatch(orders_ReaverStopWrapper,			0x004654B0, 1);
-		jmpPatch(orders_GuardWrapper,				0x00475B90, 2);
-		jmpPatch(function_004774A0_Wrapper,			0x004774A0, 3);
-	}
+void injectStopHoldPosOrdersHooks() {
+	jmpPatch(orders_MedicHoldPositionWrapper,	0x00464050, 0);
+	jmpPatch(orders_ReaverStopWrapper,			0x004654B0, 1);
+	jmpPatch(orders_CarrierStopWrapper,			0x00465910, 3);
+	jmpPatch(orders_GuardWrapper,				0x00475B90, 2);
+	jmpPatch(function_004774A0_Wrapper,			0x004774A0, 3);
+}
 
 } //hooks
