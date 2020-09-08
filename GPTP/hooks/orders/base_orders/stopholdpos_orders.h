37cbc605c6098aa76bde2f1ea4584f14ba9dc616
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-09-08 14:59:58 +0200

    Version 5.920

--- GPTP/hooks/orders/base_orders/stopholdpos_orders.h
+++ GPTP/hooks/orders/base_orders/stopholdpos_orders.h
@@ -3,12 +3,13 @@
 
 namespace hooks {
 
-	void orders_MedicHoldPosition(CUnit* unit);		//00464050  orders_MedicHoldPosition
-	void orders_ReaverStop(CUnit* unit);			//004654B0  orders_ReaverStop
-	void orders_Guard(CUnit* unit);					//00475B90  orders_Guard
-	void function_004774A0(CUnit* unit);			//004774A0	sub_4774A0_unreferenced
-	void orders_TurretGuard(CUnit* unit);			//004777F0  orders_TurretGuard
+void orders_MedicHoldPosition(CUnit* unit);		//64050
+void orders_ReaverStop(CUnit* unit);			//654B0
+void orders_CarrierStop(CUnit* unit);			//65910
+void orders_Guard(CUnit* unit);					//75B90
+void orders_PlayerGuard(CUnit* unit);			//774A0
+void orders_TurretGuard(CUnit* unit);			//777F0
 
-	void injectStopHoldPosOrdersHooks();
+void injectStopHoldPosOrdersHooks();
 
 } //namespace hooks
