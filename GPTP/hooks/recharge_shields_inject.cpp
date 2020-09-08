586f78a6c37ca01dcbe7e73f7e45329f9b763f99
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-09-08 15:17:36 +0200

    Version 5.920

--- GPTP/hooks/recharge_shields_inject.cpp
+++ GPTP/hooks/recharge_shields_inject.cpp
@@ -25,32 +25,14 @@ void __declspec(naked) unitCanRechargeShieldsWrapper() {
 
 }
 
-//Inject with callPatch
-void __declspec(naked) orderRechargeShieldsWrapper() {
-
-	static CUnit* unit;
-
-	__asm {
-		MOV unit, EDI
-		PUSHAD
-	}
-
-	hooks::orderRechargeShieldsHook(unit);
-
-	__asm {
-		POPAD
-		RETN
-	}
-
-}
+;
 
 } //unnamed namespace
 
 namespace hooks {
 
 void injectRechargeShieldsHooks() {
-	jmpPatch(unitCanRechargeShieldsWrapper,	0x00493520, 1);
-	jmpPatch(orderRechargeShieldsWrapper,	0x00493DD0, 1);
+	jmpPatch(unitCanRechargeShieldsWrapper, 0x00493520, 1);
 }
 
 } //hooks
