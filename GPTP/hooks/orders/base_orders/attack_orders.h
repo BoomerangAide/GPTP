f8062e705173b16bd549dd0201df2204ce01cd32
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-09-08 14:54:14 +0200

    Version 5.920

--- GPTP/hooks/orders/base_orders/attack_orders.h
+++ GPTP/hooks/orders/base_orders/attack_orders.h
@@ -3,15 +3,20 @@
 
 namespace hooks {
 
-void orders_ReaverAttack1(CUnit* unit);		//00465690
-void orders_CarrierAttack1(CUnit* unit);	//00465950
-void orders_TurretAttack(CUnit* unit);		//00477980
-void orders_AttackFixedRange(CUnit* unit);	//00477D00
-void orders_SapUnit(CUnit* unit);			//004788E0
-void orders_SapLocation(CUnit* unit);		//00478A40
-void orders_AttackMoveEP(CUnit* unit);		//00478DE0
-void orders_AttackMove(CUnit* unit);		//00479040
-void orders_AttackUnit(CUnit* unit);		//00479BD0
+void orders_ReaverAttack1(CUnit* unit);		//65690
+void orders_CarrierAttack1(CUnit* unit);	//65950
+void orders_StrafeUnit2(CUnit* unit);		//65E00
+void orders_StrafeUnit(CUnit* unit);		//65F60
+void orders_Reaver(CUnit* unit);			//665D0
+void orders_Carrier(CUnit* unit);			//666A0
+void orders_TurretAttack(CUnit* unit);		//77980
+void orders_AttackFixedRange(CUnit* unit);	//77D00
+void orders_SapUnit(CUnit* unit);			//788E0
+void orders_SapLocation(CUnit* unit);		//78A40
+void orders_AttackMoveEP(CUnit* unit);		//78DE0
+void orders_AttackMove(CUnit* unit);		//79040
+void orders_TowerAttack(CUnit* unit);		//79150
+void orders_AttackUnit(CUnit* unit);		//79BD0
 
 void injectAttackOrdersHooks();
 
