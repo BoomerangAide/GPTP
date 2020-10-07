8ad473ab4b542d3f326e13f2cbc57c82cb0c6b8f
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-10-07 12:23:17 +0200

    Version 5.930

--- GPTP/hooks/orders/base_orders/stopholdpos_orders.h
+++ GPTP/hooks/orders/base_orders/stopholdpos_orders.h
@@ -9,6 +9,8 @@ void orders_CarrierStop(CUnit* unit);			//65910
 void orders_Guard(CUnit* unit);					//75B90
 void orders_PlayerGuard(CUnit* unit);			//774A0
 void orders_TurretGuard(CUnit* unit);			//777F0
+void orders_HoldPosition2(CUnit* unit);			//78D10
+void makeToHoldPosition(CUnit* unit);			//EB5B0
 
 void injectStopHoldPosOrdersHooks();
 
