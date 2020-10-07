8bdc9d05177e50b97261164b0f606ba57692f371
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-10-07 12:15:00 +0200

    Version 5.930

--- GPTP/hooks/orders/larva_creep_spawn.h
+++ GPTP/hooks/orders/larva_creep_spawn.h
@@ -3,10 +3,12 @@
 
 namespace hooks {
 
-	void function_0049D660(CUnit* main_building);					//0x0049D660
-	bool function_004E8C80(CUnit* unit, CUnit* main_building);		//0x004E8C80
-	void secondaryOrd_SpawningLarva(CUnit* unit);					//0x004EA780
-	void secondaryOrd_SpreadCreepSpawningLarva(CUnit* unit);		//0x004EA880
+	void function_0049D660(CUnit* main_building);					//9D660
+	bool function_004E8C80(CUnit* unit, CUnit* main_building);		//E8C80
+	void orders_StopCreepGrowth(CUnit* unit);						//E95E0
+	void orders_InitCreepGrowth(CUnit* unit);						//E96D0	
+	void secondaryOrd_SpawningLarva(CUnit* unit);					//EA780
+	void secondaryOrd_SpreadCreepSpawningLarva(CUnit* unit);		//EA880
 
 	//The injector function that should be called in initialize.cpp
 	void injectLarvaCreepSpawnHooks();
