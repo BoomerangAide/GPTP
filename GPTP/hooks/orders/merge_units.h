6e3c4bfd7735b484b5a685f569d4fd1c83b3f92d
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-10-07 12:00:25 +0200

    Version 5.930

--- GPTP/hooks/orders/merge_units.h
+++ GPTP/hooks/orders/merge_units.h
@@ -3,11 +3,11 @@
 
 namespace hooks {
 
-	CUnit* templarMergePartner(CUnit* unit, CUnit** units_list, u32 units_list_length);	//004BFA80
+	CUnit* templarMergePartner(CUnit* unit, CUnit** units_list, u32 units_list_length);	//BFA80
 
-	void orders_CompletingArchonSummon(CUnit* unit);									//00493B10
-	void orders_WarpingDarkArchon(CUnit* unit);											//00494690
-	void orders_WarpingArchon(CUnit* unit);												//004948B0
+	void orders_CompletingArchonSummon(CUnit* unit);									//93B10
+	void orders_WarpingDarkArchon(CUnit* unit);											//94690
+	void orders_WarpingArchon(CUnit* unit);												//948B0
 
 	//The injector function that should be called in initialize.cpp
 	void injectMergeUnitsHooks();
