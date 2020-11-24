6afe987176a2b3c4e13e49efe3f6b6bab132786d
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-11-24 15:41:50 +0100

    Version 5.932

--- GPTP/hooks/orders/unit_making/unit_train.h
+++ GPTP/hooks/orders/unit_making/unit_train.h
@@ -3,8 +3,8 @@
 
 namespace hooks {
 
-	void secondaryOrd_TrainFighter(CUnit* unit);	//00466790
-	void function_00468420(CUnit* unit);			//00468420
+	void secondaryOrd_TrainFighter(CUnit* unit);	//66790
+	void function_00468420(CUnit* unit);			//68420
 
 	void injectUnitTrainHooks();
 
