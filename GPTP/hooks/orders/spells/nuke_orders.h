72c0d5ef0a06f87fa27b60f53644e727c64e12bc
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-11-24 15:39:48 +0100

    Version 5.932

--- GPTP/hooks/orders/spells/nuke_orders.h
+++ GPTP/hooks/orders/spells/nuke_orders.h
@@ -3,8 +3,12 @@
 
 namespace hooks {
 
-void orders_NukeTrack(CUnit* unit);	//00464D10
-void orders_NukeGround(CUnit* unit);//00479200
+void orders_NukePaint(CUnit* unit);		//63610
+void orders_NukeLaunch(CUnit* unit);	//64730
+void orders_NukeTrack(CUnit* unit);		//64D10
+void orders_NukeGround(CUnit* unit);	//79200
+void orders_NukeUnit(CUnit* unit);		//79410
+void orders_NukeTrain(CUnit* unit);		//E6700
 
 void injectNukeOrdersHooks();
 
