d1c1fef6f95125386092a75971b7fa29fd46ce10
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-11-24 15:41:01 +0100

    Version 5.932

--- GPTP/hooks/orders/spells/recall_spell.h
+++ GPTP/hooks/orders/spells/recall_spell.h
@@ -3,9 +3,9 @@
 
 namespace hooks {
 
-	void spells_Recall_ActionOnValidTargetForRecall(CUnit* target, CUnit* caster);	//004942D0  sub_4942D0
-	void spells_Recall_ActionOnTargetInRange(CUnit* target, CUnit* caster);			//00494400  sub_494400
-	void orders_Recall(CUnit* unit);												//00494470
+	void spells_Recall_ActionOnValidTargetForRecall(CUnit* target, CUnit* caster);	//942D0
+	void spells_Recall_ActionOnTargetInRange(CUnit* target, CUnit* caster);			//94400
+	void orders_Recall(CUnit* unit);												//94470
 
 	void injectRecallSpellHooks();
 
