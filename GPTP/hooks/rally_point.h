afb02897218f159922521cde6b66c771dcba32cf
UndeadStar <BoomerangAide@users.noreply.github.com>
2020-11-24 14:58:18 +0100

    Version 5.932

--- GPTP/hooks/rally_point.h
+++ GPTP/hooks/rally_point.h
@@ -3,9 +3,9 @@
 
 namespace hooks {
 
-void setRallyUnit(CUnit* unit, CUnit* target);			//0x00466B40
-void orderNewUnitToRally(CUnit* unit, CUnit* factory);	//0x00466F50
-void setRallyPosition(CUnit* unit, u16 x, u16 y);		//0x00466910
+void setRallyUnit(CUnit* unit, CUnit* target);			//66B40
+void orderNewUnitToRally(CUnit* unit, CUnit* factory);	//66F50
+void setRallyPosition(CUnit* unit, u16 x, u16 y);		//66910
 
 void injectRallyHooks();
 
