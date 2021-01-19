b26b3b0e5c69af7c29afb80eee0670077820bf3b
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-01-19 11:18:38 +0100

    Version 5.940b

--- GPTP/hooks/orders/research_upgrade_orders.h
+++ GPTP/hooks/orders/research_upgrade_orders.h
@@ -3,8 +3,8 @@
 
 namespace hooks {
 
-	void orders_Upgrade(CUnit* unit);		//004546A0
-	void orders_ResearchTech(CUnit* unit);	//004548B0
+	void orders_Upgrade(CUnit* unit);		//546A0
+	void orders_ResearchTech(CUnit* unit);	//548B0
 
 	void injectResearchUpgradeOrdersHooks();
 
