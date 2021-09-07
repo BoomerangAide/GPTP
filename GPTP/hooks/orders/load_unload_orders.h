e11d237b30255909be6f7b1b72763039c9a35829
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-09-07 11:26:33 +0200

    Version 5.950

--- GPTP/hooks/orders/load_unload_orders.h
+++ GPTP/hooks/orders/load_unload_orders.h
@@ -2,14 +2,14 @@
 #include "../../SCBW/structures/CUnit.h"
 
 namespace hooks {
-
-	void orders_Pickup1(CUnit* unit);		//004E7300
-	void orders_Pickup3_0(CUnit* unit);		//004E73B0
-	void orders_Pickup2(CUnit* unit);		//004E75D0
-	void orders_MoveUnload(CUnit* unit);	//004E7700
-	void orders_Pickup4_0(CUnit* unit);		//004E7B70
-	void orders_EnterTransport(CUnit* unit);//004E7CF0
-	void orders_Unload(CUnit* unit);		//004E80D0
+	
+	void orders_Pickup1(CUnit* unit);		//E7300
+	void orders_Pickup3_0(CUnit* unit);		//E73B0
+	void orders_Pickup2(CUnit* unit);		//E75D0
+	void orders_MoveUnload(CUnit* unit);	//E7700
+	void orders_Pickup4_0(CUnit* unit);		//E7B70
+	void orders_EnterTransport(CUnit* unit);//E7CF0
+	void orders_Unload(CUnit* unit);		//E80D0
 
 	void injectLoadUnloadOrdersHooks();
 
