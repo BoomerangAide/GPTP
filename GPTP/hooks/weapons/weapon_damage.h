aef0c4b7bd6810b32f98174519d2d52734133d4d
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-09-07 11:36:28 +0200

    Version 5.950

--- GPTP/hooks/weapons/weapon_damage.h
+++ GPTP/hooks/weapons/weapon_damage.h
@@ -3,13 +3,15 @@
 
 namespace hooks {
 
+void killTargetUnitCheck(CUnit* target, s32 damage, CUnit* attacker, u32 attackingPlayerId, Bool32 bNotify);	//797B0
+
 void weaponDamageHook(s32     damage,
                       CUnit*  target,
                       u8      weaponId,
                       CUnit*  attacker,
                       u8      attackingPlayerId,
                       s8      direction,
-                      u8      dmgDivisor);
+                      u8      dmgDivisor);	//79930
 
 void injectWeaponDamageHook();
 
