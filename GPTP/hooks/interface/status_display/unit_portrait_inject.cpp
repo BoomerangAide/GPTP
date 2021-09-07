a3ac0566953933985ea79b0123d2225dc6fe296e
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-09-07 11:20:35 +0200

    Version 5.950

--- GPTP/hooks/interface/status_display/unit_portrait_inject.cpp
+++ GPTP/hooks/interface/status_display/unit_portrait_inject.cpp
@@ -26,13 +26,62 @@ void __declspec(naked) setBuildingSelPortrait_Wrapper() {
 }
 
 ;
+	
+void __declspec(naked) displayUpdatePortraitHack_Wrapper() {
+
+	static const u32 JUMPBACK_POINT_DISPLAYUPDATEPORTRAITHACK = 0x0045EA40;
+
+	static u16 portraitId;
+	static CUnit* unit;
+	static u32 unkParam;
+
+	__asm {
+
+		MOV portraitId, AX
+
+		MOV EAX, [EBP+0x08]
+		MOV unit, EAX
+
+		MOV EAX, [EBP+0x0C]
+		MOV unkParam, EAX
+
+		PUSHAD
+
+	}
+
+	hooks::displayUpdatePortraitHack(&portraitId, &unit, &unkParam);
+
+	__asm {
+
+		POPAD
+
+		MOV EAX, unit
+		MOV [EBP+0x08], EAX
+
+		MOV EAX, unkParam
+		MOV [EBP+0x0C], EAX
+
+		PUSH EBX
+		PUSH ESI
+		PUSH EDI
+		MOVZX EBX, portraitId
+		MOV EAX, [0x006D5CA4]
+
+		JMP JUMPBACK_POINT_DISPLAYUPDATEPORTRAITHACK
+
+	}
+
+}
+
+;
 
 }//unnamed namespace
 
 namespace hooks {
 
-void injectUnitPortraitHook() {
-	jmpPatch(setBuildingSelPortrait_Wrapper, 0x0045E320, 6);
+void injectUnitPortraitHooks() {
+	jmpPatch(setBuildingSelPortrait_Wrapper,	0x0045E320, 6);
+	//jmpPatch(displayUpdatePortraitHack_Wrapper,	0x0045EA36, 0);
 }
 
 } //hooks
