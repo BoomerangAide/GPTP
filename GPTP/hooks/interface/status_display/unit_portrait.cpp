7af39d8de179a39b4ea7ca2b567ed7f6ca813a8c
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-09-07 11:17:53 +0200

    Version 5.950

--- GPTP/hooks/interface/status_display/unit_portrait.cpp
+++ GPTP/hooks/interface/status_display/unit_portrait.cpp
@@ -15,27 +15,62 @@ u16 setBuildingSelPortrait(CUnit* unit, u16 unitId) {
 	u16* const BW_Campaign_Advisor_PortraitId	= (u16*)0x00663024; //name is my interpretation, but really may be wrong
 	u16* const SC_Campaign_Advisor_PortraitId	= (u16*)0x006630B8; //name is my interpretation, but really may be wrong
 
-#define PORTRAIT_ID_OVERMIND		38
-#define PORTRAIT_ID_PROTOSSADVISOR	60
-
 #define SC_MISSIONS_AMOUNT	31 //including Boot Camp									//name is my interpretation, but really may be wrong
 #define BW_MISSIONS_AMOUNT	28 //including secret mission, and a mission in 2 versions	//name is my interpretation, but really may be wrong
 
 	u16 portraitId_returned = units_dat::PortraitId[unitId];
 
-	if (portraitId_returned == PORTRAIT_ID_OVERMIND) {
+	if (portraitId_returned == PortraitId::Overmind) {
 		if (*Campaign_Missions_Amount > SC_MISSIONS_AMOUNT)
 			portraitId_returned = *SC_Campaign_Advisor_PortraitId;
 	}
 	else
 	if(
-		portraitId_returned == PORTRAIT_ID_PROTOSSADVISOR &&
+		portraitId_returned == PortraitId::Protoss_Advisor &&
 		*Campaign_Missions_Amount >= BW_MISSIONS_AMOUNT
 	)
 		portraitId_returned = *BW_Campaign_Advisor_PortraitId;
 
 	return portraitId_returned;
 
+}
+
+;
+	
+//This is a hack rather than a hook, check and modify the values as you need here.
+//Testing advised, may not work the way you expect it to (like being called
+//again and again without selected unit changing).
+//Provided because there are several cases where setBuildingSelPortrait is
+//not the source of the portraitId argument.
+//
+//If unkParam is 2, will display talking portrait. Not sure of other effects
+//beyond that
+void displayUpdatePortraitHack(u16* portraitId, CUnit** unit, u32* unkParam) {
+
+	//INSERT CODE HERE AND ENABLE IN UNIT_PORTRAIT_INJECT.CPP
+
+	//Example:
+	/* Selected marines change portrait depending on their hp
+
+	if (*portraitId == PortraitId::Marine)
+	{
+
+		CUnit* _unit = *unit;
+
+		if (_unit->hitPoints > 39*256)
+			*portraitId = PortraitId::Marine;
+		else
+		if (_unit->hitPoints > 29*256)
+			*portraitId = PortraitId::Terran_Advisor;
+		else
+		if (_unit->hitPoints > 10*256)
+			*portraitId = PortraitId::Firebat;
+		else
+			*portraitId = PortraitId::Infested_Terran;
+
+	}
+	
+	/**/
 }
 
 ;
