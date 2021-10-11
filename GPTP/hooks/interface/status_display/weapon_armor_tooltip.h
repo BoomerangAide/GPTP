20340cc1f2bc06c8ab1568ed0dd92b1d455adf60
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-10-11 14:09:57 +0200

    Version 6.000

--- GPTP/hooks/interface/status_display/weapon_armor_tooltip.h
+++ GPTP/hooks/interface/status_display/weapon_armor_tooltip.h
@@ -2,6 +2,7 @@
 
 namespace hooks {
 
+//Note: none of these functions are hooked, instead the only function calling them is hooked
 const char* getWeaponTooltipString(u8 weaponId, CUnit* unit);
 const char* getArmorTooltipString(CUnit* unit);
 const char* getShieldTooltipString(CUnit* unit);
