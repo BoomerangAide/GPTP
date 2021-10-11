1305cc14242edf013adab046abd61885c3160d11
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-10-11 16:04:52 +0200

    Version 6.000

--- GPTP/hooks/transfer_tech_upgrades.h
+++ GPTP/hooks/transfer_tech_upgrades.h
@@ -3,11 +3,11 @@
 
 namespace hooks {
 
-void transferUnitTechToPlayerHook(CUnit* sourceUnit, u8 targetPlayerId, u32 researchTechFunc = 0x0047B3C0);	//0x004E2C00
-
 //Default value for researchUpgradesFunc is set as increaseUpgradeLevel @ 0047B340
 //The other possible value is sub_47B940 @ 0047B940 (called by GiveAllUnitPropertiesToPlayerAndMakeIdle)
-void transferUnitUpgradesToPlayerHook(CUnit* sourceUnit, u8 targetPlayerId, u32 researchUpgradesFunc = 0x0047B340);		//0x004E2B50
+void transferUnitUpgradesToPlayerHook(CUnit* sourceUnit, u8 targetPlayerId, u32 researchUpgradesFunc = 0x0047B340);	//E2B50
+
+void transferUnitTechToPlayerHook(CUnit* sourceUnit, u8 targetPlayerId, u32 researchTechFunc = 0x0047B3C0);			//E2C00
 
 void injectTransferTechAndUpgradesHooks();
 
