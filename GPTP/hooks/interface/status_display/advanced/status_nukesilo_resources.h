d4d8e4576915162a1a8d8a9f810a45db7dd3d33e
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-10-11 13:57:19 +0200

    Version 6.000

--- GPTP/hooks/interface/status_display/advanced/status_nukesilo_resources.h
+++ GPTP/hooks/interface/status_display/advanced/status_nukesilo_resources.h
@@ -3,8 +3,8 @@
 
 namespace hooks {
 
-void stats_resources(BinDlg* dialog);	//00426190
-void stats_nukesilo(BinDlg* dialog);	//00426FF0
+void stats_resources(BinDlg* dialog);	//26190
+void stats_nukesilo(BinDlg* dialog);	//26FF0
 
 void injectStatusNukeSilo_Resources_Hooks();
 
