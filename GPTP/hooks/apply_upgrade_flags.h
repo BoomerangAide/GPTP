34274390708d62a39f4efd82775a7a8fbea1f4ed
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-10-11 15:31:16 +0200

    Version 6.000

--- GPTP/hooks/apply_upgrade_flags.h
+++ GPTP/hooks/apply_upgrade_flags.h
@@ -1,18 +1,12 @@
-
-
 //Header file for the Apply Upgrade Flags hook module.
 #pragma once
 #include <SCBW/structures.h>
 
 namespace hooks {
 
-void applyUpgradeFlagsToNewUnitHook(CUnit* unit);
-void applyUpgradeFlagsToExistingUnitsHook(CUnit* unit, u8 upgradeId);
+void applyUpgradeFlagsToNewUnitHook(CUnit* unit);						//54370
+void applyUpgradeFlagsToExistingUnitsHook(CUnit* unit, u8 upgradeId);	//54540
 
 void injectApplyUpgradeFlags();
-
-//Wrapper for the native version of applyUpgradeFlagsToExistingUnits().
-//Applies @upgradeId to all units that are owned by the owner of @p unit.
-void applyUpgradeFlagsToExistingUnits(CUnit* unit, u8 upgradeId);
 
 } //hooks
