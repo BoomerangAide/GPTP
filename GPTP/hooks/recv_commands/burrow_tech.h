7606d41ce070f97a4d8ae238ef8597c19cdfa823
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-10-11 15:06:37 +0200

    Version 6.000

--- GPTP/hooks/recv_commands/burrow_tech.h
+++ GPTP/hooks/recv_commands/burrow_tech.h
@@ -3,9 +3,9 @@
 
 namespace hooks {
 
-void CMDRECV_Unburrow();				//004C1AC0
-void CMDRECV_Burrow(u8 bCommandType);	//004C1FA0
-void Unburrow_Proc(CUnit* unit);		//004E97C0
+void CMDRECV_Unburrow();				//C1AC0
+void CMDRECV_Burrow(u8 bCommandType);	//C1FA0
+void Unburrow_Proc(CUnit* unit);		//E97C0
 
 void injectBurrowTechHooks();
 
