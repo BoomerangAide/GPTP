08dd0c05049a443c2f07edbc5cf2ebc88bb1fff0
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-10-11 15:51:32 +0200

    Version 6.000

--- GPTP/hooks/psi_field.cpp
+++ GPTP/hooks/psi_field.cpp
@@ -5,20 +5,24 @@ namespace hooks {
 
 //Check if the given unit id can generate psi fields
 bool canMakePsiField(u16 unitId) {
-  //Default StarCraft behavior
-  if (unitId == UnitId::pylon)
-    return true;
-  return false;
+	//Default StarCraft behavior
+	if (unitId == UnitId::ProtossPylon)
+		return true;
+	return false;
 }
+
+;
 
 //Actual state check whether a unit can generate a psi field
 bool isReadyToMakePsiField(CUnit* unit) {
-  //Default StarCraft behavior
+	//Default StarCraft behavior
 
-  if (unit->id == UnitId::pylon)
-    return true;
+	if (unit->id == UnitId::ProtossPylon)
+		return true;
 
-  return false;
+	return false;
 }
+
+;
 
 } //hooks
