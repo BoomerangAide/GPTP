4e388218b86e031d2343968f5bd77e437ed5d8b9
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-10-11 15:07:25 +0200

    Version 6.000

--- GPTP/hooks/recv_commands/CMDRECV_Cancel.h
+++ GPTP/hooks/recv_commands/CMDRECV_Cancel.h
@@ -3,13 +3,13 @@
 
 namespace hooks {
 
-void CMDRECV_CancelNuke();				//004BFCD0
-void CMDRECV_CancelAddon();				//004BFF30
-void CMDRECV_CancelUpgrade();			//004BFFC0
-void CMDRECV_CancelResearch();			//004C0070
-void CMDRECV_CancelTrain(u16 wUnitID);	//004C0100
-void CMDRECV_CancelUnitMorph();			//004C2EC0
-void CMDRECV_CancelConstruction();		//004C2EF0
+void CMDRECV_CancelNuke();				//BFCD0
+void CMDRECV_CancelAddon();				//BFF30
+void CMDRECV_CancelUpgrade();			//BFFC0
+void CMDRECV_CancelResearch();			//C0070
+void CMDRECV_CancelTrain(u16 wUnitID);	//C0100
+void CMDRECV_CancelUnitMorph();			//C2EC0
+void CMDRECV_CancelConstruction();		//C2EF0
 
 void injectCMDRECV_CancelHooks();
 
