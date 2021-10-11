4f566efd926e7c5ad2d9451249fb429c66256a4e
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-10-11 13:54:33 +0200

    Version 6.000

--- GPTP/hooks/interface/status_display/advanced/status_base_text.h
+++ GPTP/hooks/interface/status_display/advanced/status_base_text.h
@@ -3,10 +3,10 @@
 
 namespace hooks {
 
-void stats_text_energy_timer(BinDlg* dialog);	//00425A30
-void stats_text_status_ranking(BinDlg* dialog);	//00425B50
-void stats_text_kills(BinDlg* dialog);			//00425DD0
-void stats_text_hp_shield(BinDlg* dialog);		//004263E0
+void stats_text_energy_timer(BinDlg* dialog);	//25A30
+void stats_text_status_ranking(BinDlg* dialog);	//25B50
+void stats_text_kills(BinDlg* dialog);			//25DD0
+void stats_text_hp_shield(BinDlg* dialog);		//263E0
 
 void injectStatusBaseTextHooks();
 
