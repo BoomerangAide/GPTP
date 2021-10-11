0b8e88a593661c5065f9c6638a823ddccc68053a
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-10-11 16:03:58 +0200

    Version 6.000

--- GPTP/hooks/transfer_tech_upgrades.cpp
+++ GPTP/hooks/transfer_tech_upgrades.cpp
@@ -17,32 +17,32 @@ struct TechTransferData {
 
 const TechTransferData techTransferData[] = {
   //Terran
-  {UnitId::marine, TechId::Stimpacks, TechId::None},
-  {UnitId::firebat, TechId::Stimpacks, TechId::None},
-  {UnitId::ghost, TechId::Lockdown, TechId::PersonnelCloaking, TechId::None},
-  {UnitId::medic, TechId::Healing, TechId::Restoration, TechId::OpticalFlare, TechId::None},
-  {UnitId::vulture, TechId::SpiderMines, TechId::None},
-  {UnitId::siege_tank, TechId::TankSiegeMode, TechId::None},
-  {UnitId::siege_tank_s, TechId::TankSiegeMode, TechId::None},
-  {UnitId::wraith, TechId::CloakingField, TechId::None},
-  {UnitId::science_vessel, TechId::DefensiveMatrix, TechId::Irradiate, TechId::EMPShockwave, TechId::None},
-  {UnitId::battlecruiser, TechId::YamatoGun, TechId::None},
+  {UnitId::TerranMarine, TechId::Stimpacks, TechId::None},
+  {UnitId::TerranFirebat, TechId::Stimpacks, TechId::None},
+  {UnitId::TerranGhost, TechId::Lockdown, TechId::PersonnelCloaking, TechId::None},
+  {UnitId::TerranMedic, TechId::Healing, TechId::Restoration, TechId::OpticalFlare, TechId::None},
+  {UnitId::TerranVulture, TechId::SpiderMines, TechId::None},
+  {UnitId::TerranSiegeTankTankMode, TechId::TankSiegeMode, TechId::None},
+  {UnitId::TerranSiegeTankSiegeMode, TechId::TankSiegeMode, TechId::None},
+  {UnitId::TerranWraith, TechId::CloakingField, TechId::None},
+  {UnitId::TerranScienceVessel, TechId::DefensiveMatrix, TechId::Irradiate, TechId::EMPShockwave, TechId::None},
+  {UnitId::TerranBattlecruiser, TechId::YamatoGun, TechId::None},
 
   //Zerg
-  {UnitId::queen, TechId::Infestation, TechId::Parasite, TechId::SpawnBroodlings, TechId::Ensnare, TechId::None},
-  {UnitId::defiler, TechId::Burrowing, TechId::DarkSwarm, TechId::Plague, TechId::Consume, TechId::None},
-  {UnitId::drone, TechId::Burrowing, TechId::None},
-  {UnitId::zergling, TechId::Burrowing, TechId::None},
-  {UnitId::hydralisk, TechId::Burrowing, TechId::LurkerAspect, TechId::None},
-  {UnitId::infested_terran, TechId::Burrowing, TechId::None},
-  {UnitId::lurker, TechId::LurkerAspect, TechId::None},
+  {UnitId::ZergQueen, TechId::Infestation, TechId::Parasite, TechId::SpawnBroodlings, TechId::Ensnare, TechId::None},
+  {UnitId::ZergDefiler, TechId::Burrowing, TechId::DarkSwarm, TechId::Plague, TechId::Consume, TechId::None},
+  {UnitId::ZergDrone, TechId::Burrowing, TechId::None},
+  {UnitId::ZergZergling, TechId::Burrowing, TechId::None},
+  {UnitId::ZergHydralisk, TechId::Burrowing, TechId::LurkerAspect, TechId::None},
+  {UnitId::ZergInfestedTerran, TechId::Burrowing, TechId::None},
+  {UnitId::ZergLurker, TechId::LurkerAspect, TechId::None},
 
   //Protoss
-  {UnitId::high_templar, TechId::ArchonWarp, TechId::PsionicStorm, TechId::Hallucination, TechId::None},
-  {UnitId::arbiter, TechId::Recall, TechId::StasisField, TechId::None},
-  {UnitId::corsair, TechId::DisruptionWeb, TechId::None},
-  {UnitId::dark_templar, TechId::DarkArchonMeld, TechId::None},
-  {UnitId::dark_archon, TechId::MindControl, TechId::Feedback, TechId::Maelstrom, TechId::None},
+  {UnitId::ProtossHighTemplar, TechId::ArchonWarp, TechId::PsionicStorm, TechId::Hallucination, TechId::None},
+  {UnitId::ProtossArbiter, TechId::Recall, TechId::StasisField, TechId::None},
+  {UnitId::ProtossCorsair, TechId::DisruptionWeb, TechId::None},
+  {UnitId::ProtossDarkTemplar, TechId::DarkArchonMeld, TechId::None},
+  {UnitId::ProtossDarkArchon, TechId::MindControl, TechId::Feedback, TechId::Maelstrom, TechId::None},
 };
 
 struct UpgradeTransferData {
@@ -52,35 +52,35 @@ struct UpgradeTransferData {
 
 const UpgradeTransferData upgradeTransferData[] = {
   //Terran
-  {UnitId::marine, UpgradeId::U_238Shells, UpgradeId::None},
-  {UnitId::ghost, UpgradeId::OcularImplants, UpgradeId::MoebiusReactor, UpgradeId::None},
-  {UnitId::medic, UpgradeId::CaduceusReactor, UpgradeId::None},
-  {UnitId::vulture, UpgradeId::IonThrusters, UpgradeId::None},
-  {UnitId::goliath, UpgradeId::CharonBooster, UpgradeId::None},
-  {UnitId::science_vessel, UpgradeId::TitanReactor, UpgradeId::None},
-  {UnitId::wraith, UpgradeId::ApolloReactor, UpgradeId::None},
-  {UnitId::battlecruiser, UpgradeId::ColossusReactor, UpgradeId::None},
+  {UnitId::TerranMarine, UpgradeId::U_238Shells, UpgradeId::None},
+  {UnitId::TerranGhost, UpgradeId::OcularImplants, UpgradeId::MoebiusReactor, UpgradeId::None},
+  {UnitId::TerranMedic, UpgradeId::CaduceusReactor, UpgradeId::None},
+  {UnitId::TerranVulture, UpgradeId::IonThrusters, UpgradeId::None},
+  {UnitId::TerranGoliath, UpgradeId::CharonBooster, UpgradeId::None},
+  {UnitId::TerranScienceVessel, UpgradeId::TitanReactor, UpgradeId::None},
+  {UnitId::TerranWraith, UpgradeId::ApolloReactor, UpgradeId::None},
+  {UnitId::TerranBattlecruiser, UpgradeId::ColossusReactor, UpgradeId::None},
 
   //Zerg
-  {UnitId::overlord, UpgradeId::VentralSacs, UpgradeId::Antennae, UpgradeId::None},
-  {UnitId::zergling, UpgradeId::MetabolicBoost, UpgradeId::AdrenalGlands, UpgradeId::None},
-  {UnitId::hydralisk, UpgradeId::MuscularAugments, UpgradeId::GroovedSpines, UpgradeId::None},
-  {UnitId::queen, UpgradeId::GameteMeiosis, UpgradeId::None},
-  {UnitId::defiler, UpgradeId::MetasynapticNode, UpgradeId::None},
-  {UnitId::ultralisk, UpgradeId::AnabolicSynthesis, UpgradeId::ChitinousPlating, UpgradeId::None},
+  {UnitId::ZergOverlord, UpgradeId::VentralSacs, UpgradeId::Antennae, UpgradeId::None},
+  {UnitId::ZergZergling, UpgradeId::MetabolicBoost, UpgradeId::AdrenalGlands, UpgradeId::None},
+  {UnitId::ZergHydralisk, UpgradeId::MuscularAugments, UpgradeId::GroovedSpines, UpgradeId::None},
+  {UnitId::ZergQueen, UpgradeId::GameteMeiosis, UpgradeId::None},
+  {UnitId::ZergDefiler, UpgradeId::MetasynapticNode, UpgradeId::None},
+  {UnitId::ZergUltralisk, UpgradeId::AnabolicSynthesis, UpgradeId::ChitinousPlating, UpgradeId::None},
 
   //Protoss
-  {UnitId::zealot, UpgradeId::LegEnhancements, UpgradeId::None},
-  {UnitId::dragoon, UpgradeId::SingularityCharge, UpgradeId::None},
-  {UnitId::high_templar, UpgradeId::KhaydarinAmulet, UpgradeId::None},
-  {UnitId::dark_archon, UpgradeId::ArgusTalisman, UpgradeId::None},
-  {UnitId::reaver, UpgradeId::ScarabDamage, UpgradeId::ReaverCapacity, UpgradeId::None},
-  {UnitId::shuttle, UpgradeId::GraviticDrive, UpgradeId::None},
-  {UnitId::observer, UpgradeId::SensorArray, UpgradeId::GraviticBoosters, UpgradeId::None},
-  {UnitId::scout, UpgradeId::ApialSensors, UpgradeId::GraviticThrusters, UpgradeId::None},
-  {UnitId::carrier, UpgradeId::CarrierCapacity, UpgradeId::None},
-  {UnitId::arbiter, UpgradeId::KhaydarinCore, UpgradeId::None},
-  {UnitId::corsair, UpgradeId::ArgusJewel, UpgradeId::None},
+  {UnitId::ProtossZealot, UpgradeId::LegEnhancements, UpgradeId::None},
+  {UnitId::ProtossDragoon, UpgradeId::SingularityCharge, UpgradeId::None},
+  {UnitId::ProtossHighTemplar, UpgradeId::KhaydarinAmulet, UpgradeId::None},
+  {UnitId::ProtossDarkArchon, UpgradeId::ArgusTalisman, UpgradeId::None},
+  {UnitId::ProtossReaver, UpgradeId::ScarabDamage, UpgradeId::ReaverCapacity, UpgradeId::None},
+  {UnitId::ProtossShuttle, UpgradeId::GraviticDrive, UpgradeId::None},
+  {UnitId::ProtossObserver, UpgradeId::SensorArray, UpgradeId::GraviticBoosters, UpgradeId::None},
+  {UnitId::ProtossScout, UpgradeId::ApialSensors, UpgradeId::GraviticThrusters, UpgradeId::None},
+  {UnitId::ProtossCarrier, UpgradeId::CarrierCapacity, UpgradeId::None},
+  {UnitId::ProtossArbiter, UpgradeId::KhaydarinCore, UpgradeId::None},
+  {UnitId::ProtossCorsair, UpgradeId::ArgusJewel, UpgradeId::None},
 };
 
 } //Unnamed namespace
@@ -106,69 +106,6 @@ bool researchHelper(u32 researchTechFunc, CUnit* sourceUnit, u32 techId, u32 rel
 	return (bPreResult != 0);
 
 } //bool researchHelper(u32 researchTechFunc, CUnit* sourceUnit, u32 techId, u32 relativeVariable, u32 destPlayerId)
-
-;
-
-//Transfers all tech related to the @p source unit to @p targetPlayerId.
-//Originally researchAllTechsFromUnit @ 004E2C00
-void transferUnitTechToPlayerHook(CUnit* sourceUnit, u8 targetPlayerId, u32 researchTechFunc) {
-
-	const TechTransferData* techDataArrayPosBegin	= &techTransferData[0];								//original was (TechTransferData*) 0x005011CA;
-	const TechTransferData* techDataArrayPosEnd		= &techTransferData[ARRAY_SIZE(techTransferData)];	//original was (TechTransferData*) 0x005012DE;
-	const int relatedTechArraySize					= 5;
-
-	//Stop if the source unit does not exist
-	if (researchTechFunc == NULL || sourceUnit == NULL)
-		SErrSetLastError(87);
-	else {
-
-		bool bBreakMainLoop = false;
-		bool bBreakSubLoop;
-
-		//Browse through TechTransferData structures
-		for (TechTransferData* data = (TechTransferData*)techDataArrayPosBegin; !bBreakMainLoop && (data != techDataArrayPosEnd); data++) {
-
-			if (sourceUnit->id == data->unitId) {
-
-				bBreakSubLoop = false;
-
-				//Browse through techs for current unit
-				for (int techIndex = 0; !bBreakSubLoop && techIndex < relatedTechArraySize; techIndex++) {
-
-					u16 techId = data->relatedTech[techIndex];
-
-					//forced end of array, stop checking techs, but still check others TechTransferData
-					//(in case the unit is represented several times?)
-					if (techId == TechId::None)
-						bBreakSubLoop = true;
-					else {
-
-						Bool8 bIsResearched;
-
-						//check if tech is researched by unit for transfer
-						if(techId >= TechId::Restoration)
-							bIsResearched = TechBw->isResearched[sourceUnit->playerId][techId - TechId::Restoration];
-						else
-							bIsResearched = TechSc->isResearched[sourceUnit->playerId][techId];
-
-						//probably research the tech of unit if not already researched by player
-						//if return false, stop checking techs and TechTransferData
-						if(!researchHelper(researchTechFunc,sourceUnit,techId,bIsResearched,targetPlayerId)) {
-							bBreakSubLoop = true;
-							bBreakMainLoop = true;			
-						}
-
-					}
-
-				}
-
-			} //if (sourceUnit->id == data->unitId)
-
-		} //for (TechTransferData* data = (TechTransferData*)techDataArrayPosBegin; !bBreakMainLoop && (data != techDataArrayPosEnd); data++)
-
-	} //if (researchTechFunc != NULL && sourceUnit != NULL)
-
-} //void transferUnitTechToPlayerHook(CUnit* sourceUnit, u8 targetPlayerId, u32 researchTechFunc)
 
 ;
 
@@ -208,7 +145,7 @@ void transferUnitUpgradesToPlayerHook(CUnit* sourceUnit, u8 targetPlayerId, u32 
 
 						u8 sourceUpgradeLevel;
 
-						//check level of upgrade of by unit for transfer
+						//check level of upgrade of source unit for transfer
 						if(upgradeId >= UpgradeId::UnusedUpgrade46)
 							sourceUpgradeLevel = UpgradesBw->currentLevel[sourceUnit->playerId][upgradeId - UpgradeId::UnusedUpgrade46];
 						else
@@ -232,6 +169,69 @@ void transferUnitUpgradesToPlayerHook(CUnit* sourceUnit, u8 targetPlayerId, u32 
 	} //if (researchupgradeFunc != NULL && sourceUnit != NULL)
 
 }
+
+;
+
+//Transfers all tech related to the @p source unit to @p targetPlayerId.
+//Originally researchAllTechsFromUnit @ 004E2C00
+void transferUnitTechToPlayerHook(CUnit* sourceUnit, u8 targetPlayerId, u32 researchTechFunc) {
+
+	const TechTransferData* techDataArrayPosBegin	= &techTransferData[0];								//original was (TechTransferData*) 0x005011CA;
+	const TechTransferData* techDataArrayPosEnd		= &techTransferData[ARRAY_SIZE(techTransferData)];	//original was (TechTransferData*) 0x005012DE;
+	const int relatedTechArraySize					= 5;
+
+	//Stop if the source unit does not exist
+	if (researchTechFunc == NULL || sourceUnit == NULL)
+		SErrSetLastError(87);
+	else {
+
+		bool bBreakMainLoop = false;
+		bool bBreakSubLoop;
+
+		//Browse through TechTransferData structures
+		for (TechTransferData* data = (TechTransferData*)techDataArrayPosBegin; !bBreakMainLoop && (data != techDataArrayPosEnd); data++) {
+
+			if (sourceUnit->id == data->unitId) {
+
+				bBreakSubLoop = false;
+
+				//Browse through techs for current unit
+				for (int techIndex = 0; !bBreakSubLoop && techIndex < relatedTechArraySize; techIndex++) {
+
+					u16 techId = data->relatedTech[techIndex];
+
+					//forced end of array, stop checking techs, but still check others TechTransferData
+					//(in case the unit is represented several times?)
+					if (techId == TechId::None)
+						bBreakSubLoop = true;
+					else {
+
+						Bool8 bIsResearched;
+
+						//check if tech is researched by source unit for transfer
+						if(techId >= TechId::Restoration)
+							bIsResearched = TechBw->isResearched[sourceUnit->playerId][techId - TechId::Restoration];
+						else
+							bIsResearched = TechSc->isResearched[sourceUnit->playerId][techId];
+
+						//probably research the tech of unit if not already researched by player
+						//if return false, stop checking techs and TechTransferData
+						if(!researchHelper(researchTechFunc,sourceUnit,techId,bIsResearched,targetPlayerId)) {
+							bBreakSubLoop = true;
+							bBreakMainLoop = true;			
+						}
+
+					}
+
+				}
+
+			} //if (sourceUnit->id == data->unitId)
+
+		} //for (TechTransferData* data = (TechTransferData*)techDataArrayPosBegin; !bBreakMainLoop && (data != techDataArrayPosEnd); data++)
+
+	} //if (researchTechFunc != NULL && sourceUnit != NULL)
+
+} //void transferUnitTechToPlayerHook(CUnit* sourceUnit, u8 targetPlayerId, u32 researchTechFunc)
 
 ;
 
