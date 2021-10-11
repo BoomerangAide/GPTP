246460553b6550480fbee5c07d31c4e142b8a9a5
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-10-11 13:59:28 +0200

    Version 6.000

--- GPTP/hooks/interface/status_display/advanced/status_transport.h
+++ GPTP/hooks/interface/status_display/advanced/status_transport.h
@@ -3,8 +3,8 @@
 
 namespace hooks {
 
-void stats_transports_loaded_units_buttons(BinDlg* dialog);	//00424BA0
-void stats_transports_cache_for_display(BinDlg* dialog);	//00424FC0
+void stats_transports_loaded_units_buttons(BinDlg* dialog);	//24BA0
+void stats_transports_cache_for_display(BinDlg* dialog);	//24FC0
 
 void injectStatusTransportHooks();
 
