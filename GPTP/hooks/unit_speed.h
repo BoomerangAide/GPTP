5c410dd1b8dc6af73f4e7350d59cffec9f81e185
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-10-11 16:11:55 +0200

    Version 6.000

--- GPTP/hooks/unit_speed.h
+++ GPTP/hooks/unit_speed.h
@@ -3,9 +3,9 @@
 
 namespace hooks {
 
-u32 getModifiedUnitSpeedHook(CUnit* unit, u32 baseSpeed);
-u32 getModifiedUnitAccelerationHook(CUnit* unit);
-u32 getModifiedUnitTurnSpeedHook(CUnit* unit);
+u32 getModifiedUnitSpeedHook(CUnit* unit, u32 baseSpeed);	//7B5F0
+u32 getModifiedUnitTurnSpeedHook(CUnit* unit);				//7B850
+u32 getModifiedUnitAccelerationHook(CUnit* unit);			//7B8A0
 
 void injectUnitSpeedHooks();
 
