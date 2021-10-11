26950825a9273e2db8d8e0bed5bbd65886faaf92
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-10-11 15:10:53 +0200

    Version 6.000

--- GPTP/hooks/recv_commands/CMDRECV_Stop.h
+++ GPTP/hooks/recv_commands/CMDRECV_Stop.h
@@ -3,9 +3,9 @@
 
 namespace hooks {
 
-void CMDRECV_ReaverStop();			//004C1240
-void CMDRECV_CarrierStop();			//004C1430
-void CMDRECV_Stop(u8 bCommandType);	//004C2190
+void CMDRECV_ReaverStop();			//C1240
+void CMDRECV_CarrierStop();			//C1430
+void CMDRECV_Stop(u8 bCommandType);	//C2190
 
 void injectCMDRECV_StopHooks();
 
