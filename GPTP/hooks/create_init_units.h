8d789f329e583bd5d147d417e37e1edb30557192
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-10-11 15:44:05 +0200

    Version 6.000

--- GPTP/hooks/create_init_units.h
+++ GPTP/hooks/create_init_units.h
@@ -3,9 +3,9 @@
 
 namespace hooks {
 
-void CreateInitialMeleeBuildings(u8 raceId, u32 playerId);	//0049D7C0
-void CreateInitialMeleeUnits();								//0049DA40
-void CreateInitialOverlord(u8 playerId);					//0049D6C0
+void CreateInitialMeleeBuildings(u8 raceId, u32 playerId);	//9D7C0
+void CreateInitialMeleeUnits();								//9DA40
+void CreateInitialOverlord(u8 playerId);					//9D6C0
 
 void injectCreateInitUnitsHooks();
 
