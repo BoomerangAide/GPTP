6a32840df1c616d876086dbd44169d5919ff1154
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-10-11 15:18:20 +0200

    Version 6.000

--- GPTP/hooks/unit_stats/weapon_range.cpp
+++ GPTP/hooks/unit_stats/weapon_range.cpp
@@ -5,6 +5,59 @@
 
 namespace hooks {
 
+/// Returns the modified max range for the weapon, which is assumed to be
+/// attached to the given unit.
+/// This hook affects the behavior of CUnit::getMaxWeaponRange().
+/// Note: Weapon ranges are measured in pixels.
+///
+/// @param	weapon		The weapons.dat ID of the weapon.
+/// @param	unit			The unit that owns the weapon. Use this to check upgrades.
+u32 getMaxWeaponRangeHook(CUnit* unit, u8 weaponId) {
+	//Default StarCraft behavior
+
+	u32 bonusAmount = 0;
+
+	//Give bonus range to units inside Bunkers
+	if (unit->status & UnitStatus::InBuilding)
+		bonusAmount = 64;
+
+	switch (unit->id) {
+		case UnitId::marine:
+			if (scbw::getUpgradeLevel(unit->playerId, UpgradeId::U_238Shells))
+				bonusAmount += 32;
+			break;
+		case UnitId::hydralisk:
+			if (scbw::getUpgradeLevel(unit->playerId, UpgradeId::GroovedSpines))
+				bonusAmount += 32;
+			break;
+		case UnitId::dragoon:
+			if (scbw::getUpgradeLevel(unit->playerId, UpgradeId::SingularityCharge))
+				bonusAmount += 64;
+			break;
+		case UnitId::fenix_dragoon:
+			if (scbw::isBroodWarMode())
+				bonusAmount += 64;
+			break;
+		case UnitId::goliath:
+		case UnitId::goliath_turret:
+			if (weaponId == WeaponId::HellfireMissilePack
+					&& scbw::getUpgradeLevel(unit->playerId, UpgradeId::CharonBooster))
+				bonusAmount += 96;
+			break;
+		case UnitId::alan_schezar:
+		case UnitId::alan_schezar_turret:
+			if (weaponId == WeaponId::HellfireMissilePack_AlanSchezar
+					&& scbw::isBroodWarMode())
+				bonusAmount += 96;
+			break;
+	}
+
+	return weapons_dat::MaxRange[weaponId] + bonusAmount;
+
+}
+
+;
+
 /// Returns the modified seek range (AKA target acquisition range) for the unit.
 /// Note: Seek ranges are measured in matrices (1 matrix = 32 pixels).
 /// This hook affects the behavior of CUnit::getSeekRange().
@@ -17,14 +70,15 @@ u8 getSeekRangeHook(CUnit* unit) {
 	//Cloaked ghosts do not voluntarily attack enemy units
 	if 
 	(
-		(		unitId == UnitId::ghost
-			 || unitId == UnitId::sarah_kerrigan
-			 || unitId == UnitId::Hero_AlexeiStukov
-			 || unitId == UnitId::Hero_SamirDuran
-			 || unitId == UnitId::Hero_InfestedDuran
-		)
-			&& unit->status & (UnitStatus::Cloaked + UnitStatus::RequiresDetection)
-			&& unit->mainOrderId != OrderId::HoldPosition2
+		(		
+			unitId == UnitId::TerranGhost ||
+			unitId == UnitId::Hero_SarahKerrigan ||
+			unitId == UnitId::Hero_AlexeiStukov ||
+			unitId == UnitId::Hero_SamirDuran ||
+			unitId == UnitId::Hero_InfestedDuran
+		) && 
+		unit->status & (UnitStatus::Cloaked + UnitStatus::RequiresDetection) &&
+		unit->mainOrderId != OrderId::HoldPosition2
 	)
 		seekRange = 0;
 	else
@@ -77,56 +131,5 @@ u8 getSeekRangeHook(CUnit* unit) {
 }
 
 ;
-
-/// Returns the modified max range for the weapon, which is assumed to be
-/// attached to the given unit.
-/// This hook affects the behavior of CUnit::getMaxWeaponRange().
-/// Note: Weapon ranges are measured in pixels.
-///
-/// @param	weapon		The weapons.dat ID of the weapon.
-/// @param	unit			The unit that owns the weapon. Use this to check upgrades.
-u32 getMaxWeaponRangeHook(CUnit* unit, u8 weaponId) {
-	//Default StarCraft behavior
-
-	u32 bonusAmount = 0;
-
-	//Give bonus range to units inside Bunkers
-	if (unit->status & UnitStatus::InBuilding)
-		bonusAmount = 64;
-
-	switch (unit->id) {
-		case UnitId::marine:
-			if (scbw::getUpgradeLevel(unit->playerId, UpgradeId::U_238Shells))
-				bonusAmount += 32;
-			break;
-		case UnitId::hydralisk:
-			if (scbw::getUpgradeLevel(unit->playerId, UpgradeId::GroovedSpines))
-				bonusAmount += 32;
-			break;
-		case UnitId::dragoon:
-			if (scbw::getUpgradeLevel(unit->playerId, UpgradeId::SingularityCharge))
-				bonusAmount += 64;
-			break;
-		case UnitId::fenix_dragoon:
-			if (scbw::isBroodWarMode())
-				bonusAmount += 64;
-			break;
-		case UnitId::goliath:
-		case UnitId::goliath_turret:
-			if (weaponId == WeaponId::HellfireMissilePack
-					&& scbw::getUpgradeLevel(unit->playerId, UpgradeId::CharonBooster))
-				bonusAmount += 96;
-			break;
-		case UnitId::alan_schezar:
-		case UnitId::alan_schezar_turret:
-			if (weaponId == WeaponId::HellfireMissilePack_AlanSchezar
-					&& scbw::isBroodWarMode())
-				bonusAmount += 96;
-			break;
-	}
-
-	return weapons_dat::MaxRange[weaponId] + bonusAmount;
-
-}
 
 } //hooks
