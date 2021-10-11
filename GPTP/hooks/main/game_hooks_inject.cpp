489c5139368f63ef7643a22635939714a1fcd931
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-10-11 14:25:23 +0200

    Version 6.000

--- GPTP/hooks/main/game_hooks_inject.cpp
+++ GPTP/hooks/main/game_hooks_inject.cpp
@@ -11,71 +11,86 @@ const u32 Hook_OnGameStart = 0x004C9A51;
 const u32 Hook_OnGameStartBack = Hook_OnGameStart + 5;
 const u32 Func_CompileSCode = 0x00417C20;
 void __declspec(naked) onGameStart() { // From BWAPI by Kovarex, slightly modified
+
 	__asm PUSHAD;
+
 	{
 		isGameOn = true;
-    hooks::gameOn();
-    GPTP::logger.startGame();
+		hooks::gameOn();
+		GPTP::logger.startGame();
 	}
+
 	__asm {
 		POPAD
 		CALL Func_CompileSCode
 		JMP Hook_OnGameStartBack
 	}
+
 }
+
+;
 
 /******** Game End ********/
 
 const u32 Func_DeleteSCode = 0x00416D90;
 void __declspec(naked) onGameEnd() { // From BWAPI by Kovarex, slightly modified
+
 	__asm PUSHAD;
+	
 	{
 		isGameOn = false;
-    hooks::gameEnd();
-    GPTP::logger.endGame();
+		hooks::gameEnd();
+		GPTP::logger.endGame();
 	}
+
 	__asm {
 		POPAD
 		CALL Func_DeleteSCode
 		RETN
 	}
+
 }
+
+;
 
 /******** NEXT FRAME HOOK ********/
 
 void __declspec(naked) nextFrameHook() { // From BWAPI by Kovarex, slightly modified
+
 	__asm {
 		PUSHAD
-		MOV EBP, ESP
-	}
-	{
-    hooks::nextFrame();
 	}
 
-  if (scbw::isGamePaused()) {
-	  __asm {
-		  POPAD
-      MOV EAX, 1
-		  RETN
-	  }
-  }
-  else {
-	  __asm {
-		  POPAD
-      MOV EAX, 0
-		  RETN
-	  }
-  }
+	{
+		hooks::nextFrame();
+	}
+
+	if (scbw::isGamePaused()) {
+		__asm {
+			POPAD
+			MOV EAX, 1
+			RETN
+		}
+	}
+	else {
+		__asm {
+			POPAD
+			MOV EAX, 0
+			RETN
+		}
+	}
 }
+
+;
 
 //-------- Hook injector --------//
 
 namespace hooks {
 
 void injectGameHooks() {
-  jmpPatch(onGameStart,     Hook_OnGameStart);
-  callPatch(onGameEnd,      0x004EE983);
-  callPatch(nextFrameHook,  0x004D974E);
+	jmpPatch(onGameStart,     Hook_OnGameStart);
+	callPatch(onGameEnd,      0x004EE983);
+	callPatch(nextFrameHook,  0x004D974E);
 }
 
 } //hooks
