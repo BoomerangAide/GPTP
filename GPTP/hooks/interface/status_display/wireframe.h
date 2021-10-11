24bb9abfb3474e537647d61fb05493f25ab445e4
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-10-11 14:13:05 +0200

    Version 6.000

--- GPTP/hooks/interface/status_display/wireframe.h
+++ GPTP/hooks/interface/status_display/wireframe.h
@@ -5,7 +5,6 @@ namespace hooks {
 
 u16 statdata_UnitWireframeUpdate_Fragment(CUnit* unit);
 
-//The injector function that should be called in initialize.cpp
 void injectWireframeHook();
 
 } //hooks
