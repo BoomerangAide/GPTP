16afd0c851e1d41c678922b409ba22b737f7fb1c
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-10-11 13:57:46 +0200

    Version 6.000

--- GPTP/hooks/interface/status_display/advanced/status_research_upgrade.h
+++ GPTP/hooks/interface/status_display/advanced/status_research_upgrade.h
@@ -3,8 +3,8 @@
 
 namespace hooks {
 
-void stats_upgrade_in_progress(BinDlg* dialog);		//00426500
-void stats_research_in_progress(BinDlg* dialog);	//004266F0
+void stats_upgrade_in_progress(BinDlg* dialog);		//26500
+void stats_research_in_progress(BinDlg* dialog);	//266F0
 
 void injectStatusResearchUpgradeHooks();
 
