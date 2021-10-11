e5c543de255dca9faa98d9d701b6cd28e26caf06
UndeadStar <BoomerangAide@users.noreply.github.com>
2021-10-11 14:37:22 +0200

    Version 6.000

--- GPTP/hooks/orders/building_making/building_protoss.h
+++ GPTP/hooks/orders/building_making/building_protoss.h
@@ -3,8 +3,8 @@
 
 namespace hooks {
 
-	void orders_ProbeBuild(CUnit* unit);	//004E4D00
-	void orders_BuildSelf2(CUnit* building);//004E4F40
+	void orders_ProbeBuild(CUnit* unit);	//E4D00
+	void orders_BuildSelf2(CUnit* building);//E4F40
 
 	void injectBuildingProtossHooks();
 
