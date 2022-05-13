d1721a97aa7e1ad390930dfb35260c73ea48a71a
UndeadStar <BoomerangAide@users.noreply.github.com>
2022-05-13 15:09:32 +0200

    Version 6.099

--- GPTP/hooks/stim_packs.cpp
+++ GPTP/hooks/stim_packs.cpp
@@ -1,5 +1,6 @@
 #include "stim_packs.h"
 #include "../SCBW/api.h"
+#include "Events/Events.h"
 
 //helper functions def
 namespace {
@@ -18,11 +19,50 @@ const int MIN_HITPOINTS_FOR_STIMPACKS = 2560; //2560 / 256 => 10 hp cost on inga
 //Local answer when using stimpacks (check units of selection,
 //and can just display an error message without notifying others
 //players of the attempt if no unit can do the stimpack action)
+#ifdef EVENTS_SYSTEM
 void CMDACT_Stimpack() {
 
 	CUnit* currentClientSelectionUnit;
 	bool bFoundUnitAbleToStimpack = false;
+	bool bCancelNormalCheck = false;
 
+	std::vector<int*> events_stimpack_attempt(1);
+
+	events_stimpack_attempt[0] = (int*)&bFoundUnitAbleToStimpack;
+
+	EventManager::EventCalling(EventId::STIMPACK_ACT_VALID_FOR_SELECTION_CHECK, &bCancelNormalCheck, events_stimpack_attempt);
+
+	if(!bCancelNormalCheck) {
+		//original code used pointer comparison with clientSelectionGroupEnd
+		//and incrementing a pointer starting at clientSelectionGroup
+		for(int i = 0; i < SELECTION_ARRAY_LENGTH && !bFoundUnitAbleToStimpack; i++) {
+
+			currentClientSelectionUnit = clientSelectionGroup->unit[i];
+
+			if(
+				currentClientSelectionUnit != NULL && 
+				currentClientSelectionUnit->hitPoints > MIN_HITPOINTS_FOR_STIMPACKS
+			) 
+				bFoundUnitAbleToStimpack = true;
+
+		}
+	}
+
+	if(bFoundUnitAbleToStimpack) {
+		static u8 opcode[] = { 0x36 };	//packet opcode for stimpack is 0x36, no others args
+		QueueGameCommand(&opcode[0],1);
+	}
+	else {
+		char* message = (char*)statTxtTbl->getString(874); //not enough life remaining
+		get_statTxt_Str_0(*activePortraitUnit,*LOCAL_NATION_ID,message);
+	}
+
+}
+#else
+void CMDACT_Stimpack() {
+
+	CUnit* currentClientSelectionUnit;
+	bool bFoundUnitAbleToStimpack = false;
 
 	//original code used pointer comparison with clientSelectionGroupEnd
 	//and incrementing a pointer starting at clientSelectionGroup
@@ -48,6 +88,7 @@ void CMDACT_Stimpack() {
 	}
 
 }
+#endif
 
 ;
 
@@ -56,21 +97,30 @@ void CMDACT_Stimpack() {
 void useStimPacksAIHook(CUnit* unit) {
 
 	const int stimTimerBase = 37;
+	bool bCancelNormalBehavior = false;
 
-	if (unit->hitPoints > MIN_HITPOINTS_FOR_STIMPACKS) {
+	std::vector<int*> events_stimpack_ai_override(1);
 
-		scbw::playSound(
-			RandBetween(SoundId::Terran_MARINE_TMaSti00_WAV, SoundId::Terran_MARINE_TMaSti01_WAV,31), 
-			unit
-		);
+	events_stimpack_ai_override[0] = (int*)unit;
 
-		unit->damageHp(MIN_HITPOINTS_FOR_STIMPACKS,NULL,-1,true);
+	EventManager::EventCalling(EventId::STIMPACK_AI_USE_OVERRIDE, &bCancelNormalBehavior, events_stimpack_ai_override);
 
-		if (unit->stimTimer < stimTimerBase) {
-			unit->stimTimer = stimTimerBase;
-			unit->updateSpeed();
+	if(!bCancelNormalBehavior) {
+		if (unit->hitPoints > MIN_HITPOINTS_FOR_STIMPACKS) {
+
+			scbw::playSound(
+				RandBetween(SoundId::Terran_MARINE_TMaSti00_WAV, SoundId::Terran_MARINE_TMaSti01_WAV,31), 
+				unit
+			);
+
+			unit->damageHp(MIN_HITPOINTS_FOR_STIMPACKS,NULL,-1,true);
+
+			if (unit->stimTimer < stimTimerBase) {
+				unit->stimTimer = stimTimerBase;
+				unit->updateSpeed();
+			}
+
 		}
-
 	}
 
 }
